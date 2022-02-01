d85aeb6bdc4bcba0fbc0a29b2cf1f3e733915dec
Nazim Bayraktar <nab1079@hotmail.com>
2022-02-01 11:55:21 +0000

    Some changes on index.html and script.js

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
-let saveBtn = document.getElementById('input-btn');
+let myLeads = [];
+const inputEl = document.getElementById('input-el');
+const saveBtn = document.getElementById('input-btn');
+const ulEl = document.getElementById('ul-el');
 
-function saveInput() {
-  console.log('Button clicked');
+saveBtn.addEventListener('click', function () {
+  myLeads.push(inputEl.value);
+  inputEl.value = '';
+  renderLeads();
+})
+
+function renderLeads() {
+  let listItems = '';
+  for (let i = 0; i < myLeads.length; i++) {
+    listItems += `
+      <li>
+        <a href='${myLeads[i]}' target='_blank'>${myLeads[i]}</a>
+      </li>
+    `
+  }
+  ulEl.innerHTML = listItems;
 }
